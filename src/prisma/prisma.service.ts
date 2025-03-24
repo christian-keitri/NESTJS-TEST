26ad090d45b3c10b849a9f5062e70b76a34e9cb8
Christian <your_email@example.com>
2025-03-24 16:57:10 +0800

    tobe/continue

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -14,4 +14,14 @@ export class PrismaService extends PrismaClient{
         });
      
     }  
+    cleanDb () {
+        return this.$transaction ([
+            this.bookmark. deleteMany(),
+            this.user. deleteMany(),
+        ]);
+        
+       
+
+    }
 }
+
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,139 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto';
+
 
  describe('App e2e', () => {
   let app: INestApplication;
-  beforeAll(async() => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-     app = moduleRef.createNestApplication();
-     app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-      }),
-     );
-     await app.init();
-  });
-    afterAll(() => {
-      app.close();
+  let prisma: PrismaService
 
-    });
+  beforeAll(async () => {   
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+  app =moduleRef.createNestApplication();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  await app.init();
+  await app.listen(3333);
 
-   it.todo('should pass');
+  prisma =app.get(PrismaService)
+  await prisma.cleanDb();
+  pactum.request.setBaseUrl('http://localhost:3333')
  });
+    afterAll(() => {
+    
+ });
+
+   describe( 'Auth', () => { 
+    const dto: AuthDto ={
+    email: 'vlad@gmail.com',
+    password: '123'
+  };
+    describe('Signup', () => {
+      it ('should throw if email empty', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signup')
+        .withBody({
+          password: dto.password,
+        }) 
+        .expectStatus(400);
+       });
+       it ('should throw if password empty', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signup')
+        .withBody({
+          emal: dto.email,
+        }) 
+        .expectStatus(400);
+       });
+       it ('should throw if no body provided', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signup')
+        .expectStatus(400);
+       });
+
+      it('should signup', () => {
+        return pactum 
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto) 
+          .expectStatus(201);
+      });
+    });
+    
+    describe('Signin', () => {
+      it ('should throw if email empty', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signin')
+        .withBody({
+          password: dto.password,
+        }) 
+        .expectStatus(400);
+       });
+       it ('should throw if password empty', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signin')
+        .withBody({
+          emal: dto.email,
+        }) 
+        .expectStatus(400);
+       });
+       it ('should throw if no body provided', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signin')
+        .expectStatus(400);
+       });
+      it('should signin', () => {
+        return pactum 
+        .spec()
+        .post('/auth/signin')
+        .withBody(dto) 
+        .expectStatus(200)
+        .stores('userAt', 'access_token')
+      });
+    }); 
+   });
+
+   describe('User', () => {
+    describe('Get me',() => {
+      it('should get current user', () =>{
+        return pactum 
+        .spec()
+        .get('/users/me')
+        .expectStatus(200);  
+      });
+    }); 
+
+    describe('Edit user', () => {});
+   });
+
+
+   describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmark', () => {});
+
+    describe('Get bookmark be id', () => {};
+  
+    describe('Edit bookmark',() => {});
+
+    describe( 'Delete bookmark'), () =>{});
+   });
+
+
+ });
+ 
